6adc22c4dd5d1b99f0eaf1a75fd81fbfa867915a
Susy-alpaca <quinuasfuentesb@hotmail>
2022-05-13 15:14:44 +0200

    primer programa en python

---
+++ hello.py
@@ -0,0 +1,6 @@
+print ("Hello World")
+print ("My name is Susy Fuentes") #show my full name
+print ("1979") # show the year that i born
+print ("I studied Biology") # say my carrier
+print ("I have a lot of respect for IT ") #some simple sentence
+print ("Bioinformatic and Biostadistic 2022")
